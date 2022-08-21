8e1c86ec3b31dddb3f7b309d930954cd1cefffc6
Sharmane <sjmburlaza@gmail.com>
2022-08-21 22:18:50 +0800

    fix index nav

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,16 @@
-let navSession = document.querySelector('#navSession');
+let navSession = document.querySelector('#indexNav');
 let registerNav = document.querySelector('#registerBtn');
 let userToken = localStorage.getItem('token');
 
 if (!userToken) {
     navSession.innerHTML = 
     `
-    <li>
-        <a href="./pages/login.html">Login</a>
-    </li>
-        
+    <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="./pages/courses.html">Courses</a></li>
+        <li><a href="./pages/about.html">About</a></li>
+        <li><a href="./pages/login.html">Login</a></li>
+    </ul>
     `
     // registerNav.innerHTML =
     // `
@@ -20,8 +22,11 @@ if (!userToken) {
 } else {
     navSession.innerHTML = 
     `
-    <li>
-        <a href="./pages/logout.html">Logout</a>
-    </li>
+    <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="./pages/courses.html">Courses</a></li>
+        <li><a href="./pages/about.html">About</a></li>
+        <li><a href="./pages/logout.html">Logout</a></li>
+    </ul>
     `
 }
