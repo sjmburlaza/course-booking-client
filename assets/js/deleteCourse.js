8792cd63b6fc1910df550bd527fc62f2fcc92095
Sharmane <sjmburlaza@gmail.com>
2022-08-21 20:10:00 +0800

    add delete functionality

---
+++ assets/js/deleteCourse.js
@@ -0,0 +1,31 @@
+let params = new URLSearchParams(window.location.search)
+let courseId = params.get('courseId')
+let token = localStorage.getItem('token')
+
+
+fetch(`https://course-booking-v2.herokuapp.com/api/courses/${courseId}`, {
+    method: 'DELETE',
+    headers: {
+        'Content-Type': 'application/json',
+        'token': `Bearer ${token}`
+    },
+    body: JSON.stringify({
+        courseId,
+        // isActive: isActive
+    })
+})
+.then(res => res.json())
+.then(data => {
+    console.log(data)
+    //deletion course successful
+    if(data === true) {
+        //redirect to course page
+        alert('Course succesfully disabled!');
+        window.location.replace("./courses.html");
+    } else {
+        alert("Something went wrong.");
+    }
+})
+
+
+
