77cfbf0f9ad530d2c6ac2efce296530b2005bc1e
Sharmane <sjmburlaza@gmail.com>
2022-09-02 21:23:26 +0800

    improve courses and course details page

--- assets/js/course.js
+++ assets/js/course.js
@@ -3,6 +3,7 @@ let courseId = params.get('courseId');
 let token = localStorage.getItem('token');
 let userId = localStorage.getItem('id');
 
+let courseImage = document.querySelector('#courseImage');
 let courseName = document.querySelector('#courseName');
 let courseDescription = document.querySelector('#courseDescription');
 let coursePrice = document.querySelector('#coursePrice');
@@ -15,39 +16,58 @@ fetch(`https://course-booking-v2.herokuapp.com/api/courses/${courseId}`)
     courseName.innerHTML = data.name;
     coursePrice.innerHTML = data.price;
     courseDescription.innerHTML = data.description;
-    enrollContainer.innerHTML =
+    courseImage.innerHTML =
+    `
+    <img src="${data.image}" height="300">
+    `
+
+    if (token) {
+        enrollContainer.innerHTML =
         `
         <button id="enrollButton" class="btn btn-info">
             Enroll
         </button>
         `
-    document.querySelector('#enrollContainer').addEventListener('click', (e) => {
-        e.preventDefault()
-
-        const isEnrolledHere = data.enrollees.some(enrollee => userId === enrollee.userId);
-
-        if (!isEnrolledHere) {
-            fetch('https://course-booking-v2.herokuapp.com/api/users/enroll', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'token': `Bearer ${token}`
-                },
-                body: JSON.stringify({
-                    courseId: courseId
+        document.querySelector('#enrollContainer').addEventListener('click', (e) => {
+            e.preventDefault()
+    
+            const isEnrolledHere = data.enrollees.some(enrollee => userId === enrollee.userId);
+    
+            if (!isEnrolledHere) {
+                fetch('https://course-booking-v2.herokuapp.com/api/users/enroll', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'token': `Bearer ${token}`
+                    },
+                    body: JSON.stringify({
+                        courseId: courseId
+                    })
                 })
-            })
-            .then(res => res.json())
-            .then(data => {
-                if (data === true) {
-                    alert('Thank you for enrolling! See you!');
-                    window.location.replace('./courses.html');
-                } else {
-                    alert('Something went wrong.');
-                }
-            })
-        } else {
-            alert('Youre already enrolled in this course!')
-        }
-    })
+                .then(res => res.json())
+                .then(data => {
+                    if (data === true) {
+                        alert('Thank you for enrolling! See you!');
+                        window.location.replace('./courses.html');
+                    } else {
+                        alert('Something went wrong.');
+                    }
+                })
+            } else {
+                alert('Youre already enrolled in this course!')
+            }
+        })
+
+    } else {
+        enrollContainer.innerHTML =
+        `
+            <a href="./login.html" class="btn btn-info">
+                Login to enroll
+            </a>
+        `
+    }
+    
+
+
+    
 })
--- assets/js/courses.js
+++ assets/js/courses.js
@@ -12,70 +12,39 @@ fetch('https://course-booking-v2.herokuapp.com/api/courses')
         courseData = 'No courses available.'
     } else {
         courseData = data.map(course => {
-            console.log(course)
 
-            if (!userToken) {
+            if (adminUser === 'true') {
+                cardFooter =
+                `
+                <div class="text-center">
+                    <a href="./enrollees.html?courseId=${course._id}"value="{course._id}" class="btn btn-info card-button">Enrollees</a>
+                    <a href="./editCourse.html?courseId=${course._id}"value="{course._id}" class="btn btn-warning card-button">Edit</a>
+                    <a href="./deleteCourse.html?courseId=${course._id}"value="{course._id}" class="btn btn-danger card-button">Disable</a>
+                </div>
+                `
+            } else {
                 cardFooter = 
                 `
-                    <a href="./courseDetails.html?courseId=${course._id}"value="{course.id}" class="btn btn-info text-white btn-block editButton">
-                        Course Details
-                    </a>
+                <a href="./course.html?courseId=${course._id}"value="{course.id}">
+                    See Course Details
+                </a>
                 `
-            } else {
-                if (!adminUser || adminUser === 'false') {
-                    cardFooter = 
-                    `
-                    <a href="./course.html?courseId=${course._id}"value="{course.id}" class="btn btn-info text-white btn-block editButton">
-                        Select Course
-                    </a>
-                    `
-                } else {
-                    cardFooter =
-                    `
-                    <div class="text-center">
-                        <a href="./enrollees.html?courseId=${course._id}"value="{course._id}" class="btn btn-info card-button">Enrollees</a>
-                        <a href="./editCourse.html?courseId=${course._id}"value="{course._id}" class="btn btn-warning card-button">Edit</a>
-                        <a href="./deleteCourse.html?courseId=${course._id}"value="{course._id}" class="btn btn-danger card-button">Disable</a>
-                    </div>
-                    `
-                }
             }
-
-            if (userToken && cardFooter) {
-                return(
-                    `
-                    <div class="col-md-6 my-3">
-                        <div class="card">
-                            <img class="card-img-top" src="${course.image}" alt="Card image cap">
-                            <div class="card-body">
-                                <h3 class="card-title">${course.name}</h3>
-                                <p class="card-text text-left">${course.description}</p>
-                                <p class="card-text text-left">Price: ₱${course.price}</p>
-                            </div>
-                            <div class="card-footer">
-                                ${cardFooter}
-                            </div>
+            return(
+                `
+                <div class="col-md-4 my-3">
+                    <div class="card">
+                        <img class="card-img-top" src="${course.image}" alt="Card image cap">
+                        <div class="card-body">
+                            <h3 class="card-title">${course.name}</h3>
+                        </div>
+                        <div class="card-footer">
+                            ${cardFooter}
                         </div>
                     </div>
-                    `
-                )
-            } else {
-                return(
-                    `
-                    <div class="col-md-6 my-3">
-                        <div class="card">
-                            <img class="card-img-top" src="${course.image}" alt="Card image cap">
-                            <div class="card-body">
-                                <h3 class="card-title">${course.name}</h3>
-                            </div>
-                            <div class="card-footer">
-                                ${cardFooter}
-                            </div>
-                        </div>
-                    </div>
-                    `
-                )
-            }
+                </div>
+                `
+            )
             
         }).join('');
     }
